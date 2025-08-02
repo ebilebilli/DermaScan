3345a229fa92e53b6d0c79cb0e7751fb1a175e76
Əbil Abilli <ebilebilli3@gmail.com>
2025-08-02 18:57:24 +0400

    ChatMessageSerializer updated

--- derma_scan/chats/serializers/chat_message_serializer.py
+++ derma_scan/chats/serializers/chat_message_serializer.py
@@ -4,6 +4,8 @@ from chats.models import ChatMessage
 
 
 class ChatMessageSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = ChatMessage
         exclude = (
@@ -17,3 +19,9 @@ class ChatMessageSerializer(serializers.ModelSerializer):
         validated_data['user'] = request.user 
         validated_data['sender'] = ChatMessage.USER
         return super().create(validated_data)
+    
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
---
+++ derma_scan/scans/serializers/__init__.py
@@ -0,0 +1,1 @@
+from .skin_image_serializer import SkinImageSerializer
